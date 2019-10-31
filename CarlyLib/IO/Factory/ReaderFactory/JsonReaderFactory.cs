5e1fbb020e8c9ecd3f30cfbe9a85bbbbfac0af9d
Carly7766 <oomori.akari7766@gmail.com>
2019-11-01 00:17:28 +0900

    add: Reader Factory

---
+++ CarlyLib/IO/Factory/ReaderFactory/JsonReaderFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CarlyLib.IO
+{
+    public class JsonReaderFactory : ReaderFactory
+    {
+        public override IReadable<T> Create<T>(string path)
+        {
+            TextReader textReader = new TextReader(path);
+            JsonReader<T> jsonReader = new JsonReader<T>(textReader); 
+            return jsonReader;
+        }
+    }
+}
---
+++ CarlyLib/IO/Factory/ReaderFactory/ReaderFactory.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CarlyLib.IO
+{
+    public abstract class ReaderFactory
+    {
+        public abstract IReadable<T> Create<T>(string path);
+    }
+}
